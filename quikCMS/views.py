9c3d0bdea973824d57a23450d61e136624ddb3e9
Gage-BCCA <gschaffer@basecampcodingacademy.org>
2024-12-27 13:19:59 -0600

    Added some styling and some placeholder buttons

--- quikCMS/views.py
+++ quikCMS/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from feed.models import Game
+
 # Create your views here.
 def cms_landing(request, id):
     target_game = get_object_or_404(Game, pk=id)
     if target_game:
         num_posts = target_game.posts_per_game.all().count()
+        num_followers = target_game.followed_games.all().count()
         context = {
             'game': target_game,
-            'num_posts': num_posts
+            'num_posts': num_posts,
+            'num_followers': num_followers
         }
         return render(request, 'quikCMS\cms_landing.html', context=context)
     return redirect('homepage')
