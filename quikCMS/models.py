eb7d4cd996955f026aade6469a1d4d3f88bcd551
Gage-BCCA <gschaffer@basecampcodingacademy.org>
2025-01-02 12:32:21 -0600

    Added date published field to devblog model

--- quikCMS/models.py
+++ quikCMS/models.py
@@ -6,8 +6,9 @@ from feed.models import Game
 class DevBlog(models.Model):
     title = models.CharField(max_length=255)
     related_game = models.ForeignKey(Game, null=False, on_delete=models.CASCADE)
-    blurb = models.TextField()
+    blurb = models.TextField(null=True)
     content = models.TextField()
+    date_published = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
